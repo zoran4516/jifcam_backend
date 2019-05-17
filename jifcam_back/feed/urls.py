d9ff1e9bbc1945bd58961fc97cec6d68f6ef4825
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-05-16 21:53:39 -0700

    Update urls.py

--- jifcam_back/feed/urls.py
+++ jifcam_back/feed/urls.py
@@ -1,11 +1,3 @@
 from django.urls import path, include
 
 from .views import TrendingView, FollowerPostView, MostPostedUsersView, UserSearchView, download
-
-urlpatterns = [
-    path('trending/', TrendingView.as_view()),
-    path('following/posts/', FollowerPostView.as_view()),
-    path('users/most/', MostPostedUsersView.as_view()),
-    path('search/user/', UserSearchView.as_view()),
-    path('download/', download )
-]
