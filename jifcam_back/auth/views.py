66a2d27712809a03ea921065156335f427bcbcc7
Edelweiss35 <45229901+Edelweiss35@users.noreply.github.com>
2019-06-04 18:29:58 -0700

    Update views.py

--- jifcam_back/auth/views.py
+++ jifcam_back/auth/views.py
@@ -1,15 +1,3 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
-from rest_framework.response import Response
 
 
 @csrf_exempt
